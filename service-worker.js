1c390b0f0992723321be49b6279dd699df9bd58f
KI-Julian <interthal.julian@gmail.com>
2026-09-15 11:07:48 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='mathemarkt-v03';
+const CACHE='mathemarkt-v032';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./apple-touch-icon.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
